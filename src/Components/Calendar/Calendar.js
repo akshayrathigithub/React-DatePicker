995aa8fca856ca5aea33f41c1be7effbd07da1c5
Akshay Rathi <akshayrathi96@gmail.com>
2020-07-03 23:51:28 +0530

    Sync

--- src/Components/Calendar/Calendar.js
+++ src/Components/Calendar/Calendar.js
@@ -1,25 +1,57 @@
-import React from 'react'
-import './Calendar.style.css'
+import React from "react"
+import "./Calendar.style.css"
 export default function Calendar() {
-    return (
-        <div id="Calendar">
-            <div className="ContentWrapper">
-                <div className="Header">
-                    <div id="Header_Heading">
-                        <p>SELECT DATE</p>
-                    </div>
-                    <div id="Header_Footer">
-                        <div id="Header_Footer_Date">
-                            <p>Mon, July 20</p>
-                        </div>
-                        <div id="Header_Footer_Edit">
-                        <i class="fas fa-pen"></i>
-                        </div>
-                    </div>
-                </div>
-                <div className="Body"></div>
-                <div className="Footer"></div>
+  return (
+    <div id="Calendar">
+      <div className="ContentWrapper">
+        <div className="Header">
+          <div id="Header_Heading">
+            <p>SELECT DATE</p>
+          </div>
+          <div id="Header_Footer">
+            <div id="Header_Footer_Date">
+              <p>Mon, July 20</p>
             </div>
+            <div id="Header_Footer_Edit">
+              <i class="fas fa-pen"></i>
+            </div>
+          </div>
         </div>
-    )
+        <div className="Body">
+          <div>1</div>
+          <div>2</div>
+          <div>3</div>
+          <div>4</div>
+          <div>5</div>
+          <div>6</div>
+          <div>7</div>
+          <div>8</div>
+          <div>9</div>
+          <div>10</div>
+          <div>11</div>
+          <div>12</div>
+          <div>13</div>
+          <div>14</div>
+          <div>15</div>
+          <div>16</div>
+          <div>17</div>
+          <div>18</div>
+          <div>19</div>
+          <div>20</div>
+          <div>21</div>
+          <div>22</div>
+          <div>23</div>
+          <div>24</div>
+          <div>25</div>
+          <div>26</div>
+          <div>27</div>
+          <div>28</div>
+          <div>29</div>
+          <div>30</div>
+          <div>31</div>
+        </div>
+        <div className="Footer"></div>
+      </div>
+    </div>
+  )
 }
