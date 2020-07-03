5ba4a979ebb367f73fb117fd4ca200060de812fb
Akshay Rathi <akshayrathi96@gmail.com>
2020-07-03 18:47:43 +0530

    Basic Setup of project

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
+import Calendar from './Calendar/Calendar'
 
 export default class App extends Component {
     render() {
         return (
             <div>
-                This is the App Component !!!
+                <Calendar />
             </div>
         )
     }
