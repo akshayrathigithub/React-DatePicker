e51b10d03c35982d64f443ba7995d6bdc4195de4
Akshay Rathi <akshayrathi96@gmail.com>
2020-07-05 16:48:13 +0530

    transition added

--- src/Components/Calendar/Calendar.js
+++ src/Components/Calendar/Calendar.js
@@ -1,6 +1,20 @@
-import React from "react"
+import React, { useRef } from "react"
 import "./Calendar.style.css"
 export default function Calendar() {
+  const BodyWrapper = useRef(null)
+  const Called = () => {
+    let slider = BodyWrapper.current
+    slider.style.transform = "translate3d(-20%, 0%, 0px)"
+    slider.addEventListener("transitionend", () => {
+      slider.appendChild(slider.firstElementChild)
+      slider.style.transition = "none"
+      slider.style.transform = "translate3d(0%, 0%, 0px)"
+      setTimeout(() => {
+        slider.style.transform = "transform 0.8s ease-in-out"
+        console.log(slider.style.transform)
+      })
+    })
+  }
   return (
     <div id="Calendar">
       <div className="ContentWrapper">
@@ -13,64 +27,291 @@ export default function Calendar() {
               <p>Mon, July 20</p>
             </div>
             <div id="Header_Footer_Edit">
-              <i class="fas fa-pen"></i>
+              <i className="fas fa-pen"></i>
             </div>
           </div>
         </div>
-        <div className="BodyWrapper">
-          <div className="Body">
-            <div className="Body_Header">
-              <div className="Body_Header_Date">
-                <p>December 1979</p>
+        <div className="Body">
+          <div className="BodyWrapper" ref={BodyWrapper}>
+            <div className="BodyChild">
+              <div className="Body_Header">
+                <div className="Body_Header_Date">
+                  <p>December 1979</p>
+                </div>
+                <div className="Body_Header_DropDown">
+                  <i onClick={Called} className="fas fa-chevron-down"></i>
+                </div>
+                <div className="Body_Header_Left">
+                  <i className="fas fa-chevron-left"></i>
+                </div>
+                <div className="Body_Header_Right">
+                  <i className="fas fa-chevron-right"></i>
+                </div>
               </div>
-              <div className="Body_Header_DropDown"><i class="fas fa-chevron-down"></i>
-              </div>
-              <div className="Body_Header_Left">
-                <i class="fas fa-chevron-left"></i>
-              </div>
-              <div className="Body_Header_Right">
-                <i class="fas fa-chevron-right"></i>
+              <div className="Body_Days">
+                <div>Mon</div>
+                <div>Tue</div>
+                <div>Wed</div>
+                <div>Thu</div>
+                <div>Fri</div>
+                <div>Sat</div>
+                <div>Sun</div>
+                <div>1</div>
+                <div>2</div>
+                <div>3</div>
+                <div>4</div>
+                <div>5</div>
+                <div>6</div>
+                <div>7</div>
+                <div>8</div>
+                <div>9</div>
+                <div>10</div>
+                <div>11</div>
+                <div>12</div>
+                <div>13</div>
+                <div>14</div>
+                <div>15</div>
+                <div>16</div>
+                <div>17</div>
+                <div>18</div>
+                <div>19</div>
+                <div>20</div>
+                <div>21</div>
+                <div>22</div>
+                <div>23</div>
+                <div>24</div>
+                <div>25</div>
+                <div>26</div>
+                <div>27</div>
+                <div>28</div>
+                <div>29</div>
+                <div>30</div>
+                <div>31</div>
               </div>
             </div>
-            <div className="Body_Days">
-              <div>Mon</div>
-              <div>Tue</div>
-              <div>Wed</div>
-              <div>Thu</div>
-              <div>Fri</div>
-              <div>Sat</div>
-              <div>Sun</div>
-              <div>1</div>
-              <div>2</div>
-              <div>3</div>
-              <div>4</div>
-              <div>5</div>
-              <div>6</div>
-              <div>7</div>
-              <div>8</div>
-              <div>9</div>
-              <div>10</div>
-              <div>11</div>
-              <div>12</div>
-              <div>13</div>
-              <div>14</div>
-              <div>15</div>
-              <div>16</div>
-              <div>17</div>
-              <div>18</div>
-              <div>19</div>
-              <div>20</div>
-              <div>21</div>
-              <div>22</div>
-              <div>23</div>
-              <div>24</div>
-              <div>25</div>
-              <div>26</div>
-              <div>27</div>
-              <div>28</div>
-              <div>29</div>
-              <div>30</div>
-              <div>31</div>
+            <div className="BodyChild">
+              <div className="Body_Header">
+                <div className="Body_Header_Date">
+                  <p>January 1979</p>
+                </div>
+                <div className="Body_Header_DropDown">
+                  <i onClick={Called} className="fas fa-chevron-down"></i>
+                </div>
+                <div className="Body_Header_Left">
+                  <i className="fas fa-chevron-left"></i>
+                </div>
+                <div className="Body_Header_Right">
+                  <i className="fas fa-chevron-right"></i>
+                </div>
+              </div>
+              <div className="Body_Days">
+                <div>Mon</div>
+                <div>Tue</div>
+                <div>Wed</div>
+                <div>Thu</div>
+                <div>Fri</div>
+                <div>Sat</div>
+                <div>Sun</div>
+                <div>1</div>
+                <div>2</div>
+                <div>3</div>
+                <div>4</div>
+                <div>5</div>
+                <div>6</div>
+                <div>7</div>
+                <div>8</div>
+                <div>9</div>
+                <div>10</div>
+                <div>11</div>
+                <div>12</div>
+                <div>13</div>
+                <div>14</div>
+                <div>15</div>
+                <div>16</div>
+                <div>17</div>
+                <div>18</div>
+                <div>19</div>
+                <div>20</div>
+                <div>21</div>
+                <div>22</div>
+                <div>23</div>
+                <div>24</div>
+                <div>25</div>
+                <div>26</div>
+                <div>27</div>
+                <div>28</div>
+                <div>29</div>
+                <div>30</div>
+                <div>31</div>
+              </div>
+            </div>
+            <div className="BodyChild">
+              <div className="Body_Header">
+                <div className="Body_Header_Date">
+                  <p>Feb 1979</p>
+                </div>
+                <div className="Body_Header_DropDown">
+                  <i onClick={Called} className="fas fa-chevron-down"></i>
+                </div>
+                <div className="Body_Header_Left">
+                  <i className="fas fa-chevron-left"></i>
+                </div>
+                <div className="Body_Header_Right">
+                  <i className="fas fa-chevron-right"></i>
+                </div>
+              </div>
+              <div className="Body_Days">
+                <div>Mon</div>
+                <div>Tue</div>
+                <div>Wed</div>
+                <div>Thu</div>
+                <div>Fri</div>
+                <div>Sat</div>
+                <div>Sun</div>
+                <div>1</div>
+                <div>2</div>
+                <div>3</div>
+                <div>4</div>
+                <div>5</div>
+                <div>6</div>
+                <div>7</div>
+                <div>8</div>
+                <div>9</div>
+                <div>10</div>
+                <div>11</div>
+                <div>12</div>
+                <div>13</div>
+                <div>14</div>
+                <div>15</div>
+                <div>16</div>
+                <div>17</div>
+                <div>18</div>
+                <div>19</div>
+                <div>20</div>
+                <div>21</div>
+                <div>22</div>
+                <div>23</div>
+                <div>24</div>
+                <div>25</div>
+                <div>26</div>
+                <div>27</div>
+                <div>28</div>
+                <div>29</div>
+                <div>30</div>
+                <div>31</div>
+              </div>
+            </div>
+            <div className="BodyChild">
+              <div className="Body_Header">
+                <div className="Body_Header_Date">
+                  <p>Mar 1979</p>
+                </div>
+                <div className="Body_Header_DropDown">
+                  <i onClick={Called} className="fas fa-chevron-down"></i>
+                </div>
+                <div className="Body_Header_Left">
+                  <i className="fas fa-chevron-left"></i>
+                </div>
+                <div className="Body_Header_Right">
+                  <i className="fas fa-chevron-right"></i>
+                </div>
+              </div>
+              <div className="Body_Days">
+                <div>Mon</div>
+                <div>Tue</div>
+                <div>Wed</div>
+                <div>Thu</div>
+                <div>Fri</div>
+                <div>Sat</div>
+                <div>Sun</div>
+                <div>1</div>
+                <div>2</div>
+                <div>3</div>
+                <div>4</div>
+                <div>5</div>
+                <div>6</div>
+                <div>7</div>
+                <div>8</div>
+                <div>9</div>
+                <div>10</div>
+                <div>11</div>
+                <div>12</div>
+                <div>13</div>
+                <div>14</div>
+                <div>15</div>
+                <div>16</div>
+                <div>17</div>
+                <div>18</div>
+                <div>19</div>
+                <div>20</div>
+                <div>21</div>
+                <div>22</div>
+                <div>23</div>
+                <div>24</div>
+                <div>25</div>
+                <div>26</div>
+                <div>27</div>
+                <div>28</div>
+                <div>29</div>
+                <div>30</div>
+                <div>31</div>
+              </div>
+            </div>
+            <div className="BodyChild">
+              <div className="Body_Header">
+                <div className="Body_Header_Date">
+                  <p>June 1979</p>
+                </div>
+                <div className="Body_Header_DropDown">
+                  <i onClick={Called} className="fas fa-chevron-down"></i>
+                </div>
+                <div className="Body_Header_Left">
+                  <i className="fas fa-chevron-left"></i>
+                </div>
+                <div className="Body_Header_Right">
+                  <i className="fas fa-chevron-right"></i>
+                </div>
+              </div>
+              <div className="Body_Days">
+                <div>Mon</div>
+                <div>Tue</div>
+                <div>Wed</div>
+                <div>Thu</div>
+                <div>Fri</div>
+                <div>Sat</div>
+                <div>Sun</div>
+                <div>1</div>
+                <div>2</div>
+                <div>3</div>
+                <div>4</div>
+                <div>5</div>
+                <div>6</div>
+                <div>7</div>
+                <div>8</div>
+                <div>9</div>
+                <div>10</div>
+                <div>11</div>
+                <div>12</div>
+                <div>13</div>
+                <div>14</div>
+                <div>15</div>
+                <div>16</div>
+                <div>17</div>
+                <div>18</div>
+                <div>19</div>
+                <div>20</div>
+                <div>21</div>
+                <div>22</div>
+                <div>23</div>
+                <div>24</div>
+                <div>25</div>
+                <div>26</div>
+                <div>27</div>
+                <div>28</div>
+                <div>29</div>
+                <div>30</div>
+                <div>31</div>
+              </div>
             </div>
           </div>
         </div>
