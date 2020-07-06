aa5fe680ec6e361db8faef85dfeeb34b848578b2
Akshay Rathi <akshayrathi96@gmail.com>
2020-07-06 20:15:34 +0530

    Days rendered dynamically

--- src/Components/Calendar/Calendar.js
+++ src/Components/Calendar/Calendar.js
@@ -7,10 +7,25 @@ export default function Calendar() {
   const [Direction, setDirection] = useState(false)
   const [EditWindow, setEditWindow] = useState(false)
   const [selectedDate, setSelectedDate] = useState("")
+  const Months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ]
 
+  let todayDate = new Date()
+  console.log(todayDate)
   const Slide = (action) => {
     let slider = Body.current.firstElementChild
-    console.log(Direction)
     if (action === "Forward") {
       slider.style.transform = "translate3d(-60%, 0%, 0px)"
       setDirection(true)
@@ -24,20 +39,15 @@ export default function Calendar() {
   }
   const EditCalled = () => {
     let slider = Body.current.firstElementChild
+    slider.style.transition = "none"
     if (!EditWindow) {
-      slider.style.transition = "none"
       slider.style.transform = "translate3d(0%, 0%, 0px)"
-      setTimeout(() => {
-        slider.style.transition = "transform 0.8s ease-in-out"
-      },3)
     } else {
-      slider.style.transition = "none"
       slider.style.transform = "translate3d(-40%, 0%, 0px)"
-      setTimeout(() => {
-        slider.style.transition = "transform 0.8s ease-in-out"
-      },3)
-      console.log(123)
     }
+    setTimeout(() => {
+      slider.style.transition = "transform 0.8s ease-in-out"
+    }, 15)
     setEditWindow(!EditWindow)
   }
   const transitionChange = () => {
@@ -53,6 +63,7 @@ export default function Calendar() {
       slider.style.transition = "transform 0.8s ease-in-out"
     })
   }
+  console.log("Hello")
   return (
     <div id="Calendar">
       <div className="ContentWrapper">
@@ -69,7 +80,7 @@ export default function Calendar() {
             </div>
           </div>
         </div>
-        <BodyDiv Height={EditWindow ? '16' : '70'} ref={Body}>
+        <BodyDiv Height={EditWindow ? "16" : "70"} ref={Body}>
           {EditWindow ? (
             <div className="ManualDate">
               <div className="ManualDateWrapper">
@@ -88,286 +99,38 @@ export default function Calendar() {
             </div>
           ) : (
             <div className="BodyWrapper" onTransitionEnd={transitionChange}>
-              <div className="BodyChild">
-                <div className="Body_Header">
-                  <div className="Body_Header_Date">
-                    <p>Feb 1979</p>
+              {[...Array(5)].map((child, ind)=>{
+                return (
+                  <div className="BodyChild" key={ind}>
+                    <div className="Body_Header">
+                      <div className="Body_Header_Date">
+                        <p>{} 1979</p>
+                      </div>
+                      <div className="Body_Header_DropDown">
+                        <i className="fas fa-chevron-down"></i>
+                      </div>
+                      <div onClick={() => Slide("Backward")} className="Body_Header_Left">
+                        <i className="fas fa-chevron-left"></i>
+                      </div>
+                      <div className="Body_Header_Right">
+                        <i onClick={() => Slide("Forward")} className="fas fa-chevron-right"></i>
+                      </div>
+                    </div>
+                    <div className="Body_Days">
+                      <div>Mon</div>
+                      <div>Tue</div>
+                      <div>Wed</div>
+                      <div>Thu</div>
+                      <div>Fri</div>
+                      <div>Sat</div>
+                      <div>Sun</div>
+                      {[...Array(30)].map((days, i) =>{
+                        return(<div key={i}>{i+1}</div>)
+                      })}
+                    </div>
                   </div>
-                  <div className="Body_Header_DropDown">
-                    <i className="fas fa-chevron-down"></i>
-                  </div>
-                  <div onClick={() => Slide("Backward")} className="Body_Header_Left">
-                    <i className="fas fa-chevron-left"></i>
-                  </div>
-                  <div className="Body_Header_Right">
-                    <i onClick={() => Slide("Forward")} className="fas fa-chevron-right"></i>
-                  </div>
-                </div>
-                <div className="Body_Days">
-                  <div>Mon</div>
-                  <div>Tue</div>
-                  <div>Wed</div>
-                  <div>Thu</div>
-                  <div>Fri</div>
-                  <div>Sat</div>
-                  <div>Sun</div>
-                  <div>1</div>
-                  <div>2</div>
-                  <div>3</div>
-                  <div>4</div>
-                  <div>5</div>
-                  <div>6</div>
-                  <div>7</div>
-                  <div>8</div>
-                  <div>9</div>
-                  <div>10</div>
-                  <div>11</div>
-                  <div>12</div>
-                  <div>13</div>
-                  <div>14</div>
-                  <div>15</div>
-                  <div>16</div>
-                  <div>17</div>
-                  <div>18</div>
-                  <div>19</div>
-                  <div>20</div>
-                  <div>21</div>
-                  <div>22</div>
-                  <div>23</div>
-                  <div>24</div>
-                  <div>25</div>
-                  <div>26</div>
-                  <div>27</div>
-                  <div>28</div>
-                  <div>29</div>
-                  <div>30</div>
-                  <div>31</div>
-                </div>
-              </div>
-              <div className="BodyChild">
-                <div className="Body_Header">
-                  <div className="Body_Header_Date">
-                    <p>Mar 1979</p>
-                  </div>
-                  <div className="Body_Header_DropDown">
-                    <i className="fas fa-chevron-down"></i>
-                  </div>
-                  <div onClick={() => Slide("Backward")} className="Body_Header_Left">
-                    <i className="fas fa-chevron-left"></i>
-                  </div>
-                  <div className="Body_Header_Right">
-                    <i onClick={() => Slide("Forward")} className="fas fa-chevron-right"></i>
-                  </div>
-                </div>
-                <div className="Body_Days">
-                  <div>Mon</div>
-                  <div>Tue</div>
-                  <div>Wed</div>
-                  <div>Thu</div>
-                  <div>Fri</div>
-                  <div>Sat</div>
-                  <div>Sun</div>
-                  <div>1</div>
-                  <div>2</div>
-                  <div>3</div>
-                  <div>4</div>
-                  <div>5</div>
-                  <div>6</div>
-                  <div>7</div>
-                  <div>8</div>
-                  <div>9</div>
-                  <div>10</div>
-                  <div>11</div>
-                  <div>12</div>
-                  <div>13</div>
-                  <div>14</div>
-                  <div>15</div>
-                  <div>16</div>
-                  <div>17</div>
-                  <div>18</div>
-                  <div>19</div>
-                  <div>20</div>
-                  <div>21</div>
-                  <div>22</div>
-                  <div>23</div>
-                  <div>24</div>
-                  <div>25</div>
-                  <div>26</div>
-                  <div>27</div>
-                  <div>28</div>
-                  <div>29</div>
-                  <div>30</div>
-                  <div>31</div>
-                </div>
-              </div>
-              <div className="BodyChild">
-                <div className="Body_Header">
-                  <div className="Body_Header_Date">
-                    <p>April 1979</p>
-                  </div>
-                  <div className="Body_Header_DropDown">
-                    <i className="fas fa-chevron-down"></i>
-                  </div>
-                  <div onClick={() => Slide("Backward")} className="Body_Header_Left">
-                    <i className="fas fa-chevron-left"></i>
-                  </div>
-                  <div className="Body_Header_Right">
-                    <i onClick={() => Slide("Forward")} className="fas fa-chevron-right"></i>
-                  </div>
-                </div>
-                <div className="Body_Days">
-                  <div>Mon</div>
-                  <div>Tue</div>
-                  <div>Wed</div>
-                  <div>Thu</div>
-                  <div>Fri</div>
-                  <div>Sat</div>
-                  <div>Sun</div>
-                  <div>1</div>
-                  <div>2</div>
-                  <div>3</div>
-                  <div>4</div>
-                  <div>5</div>
-                  <div>6</div>
-                  <div>7</div>
-                  <div>8</div>
-                  <div>9</div>
-                  <div>10</div>
-                  <div>11</div>
-                  <div>12</div>
-                  <div>13</div>
-                  <div>14</div>
-                  <div>15</div>
-                  <div>16</div>
-                  <div>17</div>
-                  <div>18</div>
-                  <div>19</div>
-                  <div>20</div>
-                  <div>21</div>
-                  <div>22</div>
-                  <div>23</div>
-                  <div>24</div>
-                  <div>25</div>
-                  <div>26</div>
-                  <div>27</div>
-                  <div>28</div>
-                  <div>29</div>
-                  <div>30</div>
-                  <div>31</div>
-                </div>
-              </div>
-              <div className="BodyChild">
-                <div className="Body_Header">
-                  <div className="Body_Header_Date">
-                    <p>May 1979</p>
-                  </div>
-                  <div className="Body_Header_DropDown">
-                    <i className="fas fa-chevron-down"></i>
-                  </div>
-                  <div onClick={() => Slide("Backward")} className="Body_Header_Left">
-                    <i className="fas fa-chevron-left"></i>
-                  </div>
-                  <div className="Body_Header_Right">
-                    <i onClick={() => Slide("Forward")} className="fas fa-chevron-right"></i>
-                  </div>
-                </div>
-                <div className="Body_Days">
-                  <div>Mon</div>
-                  <div>Tue</div>
-                  <div>Wed</div>
-                  <div>Thu</div>
-                  <div>Fri</div>
-                  <div>Sat</div>
-                  <div>Sun</div>
-                  <div>1</div>
-                  <div>2</div>
-                  <div>3</div>
-                  <div>4</div>
-                  <div>5</div>
-                  <div>6</div>
-                  <div>7</div>
-                  <div>8</div>
-                  <div>9</div>
-                  <div>10</div>
-                  <div>11</div>
-                  <div>12</div>
-                  <div>13</div>
-                  <div>14</div>
-                  <div>15</div>
-                  <div>16</div>
-                  <div>17</div>
-                  <div>18</div>
-                  <div>19</div>
-                  <div>20</div>
-                  <div>21</div>
-                  <div>22</div>
-                  <div>23</div>
-                  <div>24</div>
-                  <div>25</div>
-                  <div>26</div>
-                  <div>27</div>
-                  <div>28</div>
-                  <div>29</div>
-                  <div>30</div>
-                  <div>31</div>
-                </div>
-              </div>
-              <div className="BodyChild">
-                <div className="Body_Header">
-                  <div className="Body_Header_Date">
-                    <p>June 1979</p>
-                  </div>
-                  <div className="Body_Header_DropDown">
-                    <i className="fas fa-chevron-down"></i>
-                  </div>
-                  <div onClick={() => Slide("Backward")} className="Body_Header_Left">
-                    <i className="fas fa-chevron-left"></i>
-                  </div>
-                  <div className="Body_Header_Right">
-                    <i onClick={() => Slide("Forward")} className="fas fa-chevron-right"></i>
-                  </div>
-                </div>
-                <div className="Body_Days">
-                  <div>Mon</div>
-                  <div>Tue</div>
-                  <div>Wed</div>
-                  <div>Thu</div>
-                  <div>Fri</div>
-                  <div>Sat</div>
-                  <div>Sun</div>
-                  <div>1</div>
-                  <div>2</div>
-                  <div>3</div>
-                  <div>4</div>
-                  <div>5</div>
-                  <div>6</div>
-                  <div>7</div>
-                  <div>8</div>
-                  <div>9</div>
-                  <div>10</div>
-                  <div>11</div>
-                  <div>12</div>
-                  <div>13</div>
-                  <div>14</div>
-                  <div>15</div>
-                  <div>16</div>
-                  <div>17</div>
-                  <div>18</div>
-                  <div>19</div>
-                  <div>20</div>
-                  <div>21</div>
-                  <div>22</div>
-                  <div>23</div>
-                  <div>24</div>
-                  <div>25</div>
-                  <div>26</div>
-                  <div>27</div>
-                  <div>28</div>
-                  <div>29</div>
-                  <div>30</div>
-                  <div>31</div>
-                </div>
-              </div>
+                )
+              })}
             </div>
           )}
         </BodyDiv>
